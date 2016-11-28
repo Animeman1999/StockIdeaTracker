130c0ccb89a236377322d3bf5d4bb47dba2d9dca
Jeffrey Martin <animeman1999@gmail.com>
2016-11-27 19:10:04 -0500

    Adds menu items

--- StockIdeaTracker/app/src/main/java/com/example/jmartin5229/stockideatracker/MainActivity.java
+++ StockIdeaTracker/app/src/main/java/com/example/jmartin5229/stockideatracker/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.jmartin5229.stockideatracker;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mSubtitleVisible = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +21,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+//        MenuItem subtitleItem =
+//                menu.findItem(R.id.menu_item_show_subtitle);
+//        switch (mSubtitleVisible)
+//        {
+//            case 1:
+//            subtitleItem.setTitle(R.string.list_of_stock_ideas);
+//                return true;
+//            case 2:
+//            subtitleItem.setTitle(R.string.add_new_stock_idea);
+//                return true;
+//            case 3:
+//                subtitleItem.setTitle(R.string.portfolio_summary);
+//                return true;
+//        }
+        updateSubtitle();
         return true;
     }
 
@@ -31,11 +51,41 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id)
+        {
+            case  R.id.list_of_stock_ideas:
+                mSubtitleVisible = 1;
+                break;
+            case R.id.add_new_stock_idea:
+                mSubtitleVisible = 2;
+                break;
+            case R.id.portfolio_summary:
+                mSubtitleVisible = 3;
+                break;
         }
+        updateSubtitle();
 
         return super.onOptionsItemSelected(item);
     }
+    private void updateSubtitle() {
+
+        String subtitle = getString(R.string.list_of_stock_ideas);
+        switch (mSubtitleVisible) {
+            case 1:
+                subtitle = getString(R.string.list_of_stock_ideas);
+                break;
+            case 2:
+                subtitle = getString(R.string.add_new_stock_idea);
+                break;
+            case 3:
+                subtitle = getString(R.string.portfolio_summary);
+                break;
+        }
+        AppCompatActivity activity = (AppCompatActivity) this;
+        activity.getSupportActionBar().setSubtitle(subtitle);
+    }
+
+
 }
