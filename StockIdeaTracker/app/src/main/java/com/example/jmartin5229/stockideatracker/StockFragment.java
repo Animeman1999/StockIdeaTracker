82eb4bfc08305505ffa4613a565ef6a3aa75e67c
Jeffrey Martin <animeman1999@gmail.com>
2016-12-03 00:15:22 -0500

    Wires up all the changeable items in the view to the StockFragment

--- StockIdeaTracker/app/src/main/java/com/example/jmartin5229/stockideatracker/StockFragment.java
+++ StockIdeaTracker/app/src/main/java/com/example/jmartin5229/stockideatracker/StockFragment.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.jmartin5229.stockideatracker.StockApi;
 
@@ -27,6 +29,17 @@ public class StockFragment extends Fragment {
 
     private Stock mStock;
     private EditText mIdeaTitle;
+    private TextView mTicker;
+    private TextView mDateAdded;
+    private EditText mDescription;
+    private ImageView mPicture;
+    private TextView mGPS;
+    private TextView mPurchasePrice;
+    private TextView mNumberStock;
+    private TextView mTotalStockPrice;
+    private TextView mCurrentStockPrice;
+    private TextView mCurrentStockValue;
+
 
     public static StockFragment newInstance(UUID stockID)
     {
@@ -70,8 +83,58 @@ public class StockFragment extends Fragment {
             }
         });
 
-        
+        mTicker = (TextView)v.findViewById(R.id.stock_idea_ticker);
+        mTicker.setText(mStock.getTicker());
 
+        mDateAdded = (TextView)v.findViewById(R.id.stock_idea_creation_date);
+        mDateAdded.setText(mStock.getCreationDate().toString());
+
+        mDescription =(EditText)v.findViewById(R.id.stock_idea_description);
+        mDescription.setText(mStock.getDescription());
+        mDescription.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mStock.setDescription(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mPicture = (ImageView)v.findViewById(R.id.list_item_stock_idea_thumbnail);
+        String imageName = mStock.getPicture();
+        //This needs to be tested****************************************************************************************
+        int pictureId =  getResources().getIdentifier(imageName, "drawable", "com.example.jmartin5229.stockideatracker");
+        mPicture.setImageResource(pictureId);
+
+        mGPS = (TextView)v.findViewById(R.id.stock_idea_coordinates);
+        mGPS.setText(mStock.getCoordinates());
+
+        mPurchasePrice = (TextView)v.findViewById(R.id.stock_idea_purchase_price);
+        Double purchasePrice = mStock.getPurchasePrice();
+        mPurchasePrice.setText(String.valueOf( purchasePrice));
+
+        mNumberStock = (TextView)v.findViewById(R.id.stock_idea_number_of_stock_purchased);
+        int numberStock = mStock.getNumberStock();
+        mNumberStock.setText(String.valueOf(numberStock));
+
+        mTotalStockPrice = (TextView)v.findViewById(R.id.stock_idea_total_purchase_price);
+        mTotalStockPrice.setText(String.valueOf (purchasePrice * numberStock));
+
+        mCurrentStockPrice = (TextView)v.findViewById(R.id.stock_idea_current_price);
+        Double currentPrice = 1.11; //88888888888888888888888888Needs to be a method here to get the price from yahoo
+        mCurrentStockPrice.setText(String.valueOf(currentPrice));
+
+        mCurrentStockValue = (TextView)v.findViewById(R.id.stock_idea_total_current_value);
+        mCurrentStockValue.setText(String.valueOf(currentPrice * numberStock));
         return v;
     }
+
 }
